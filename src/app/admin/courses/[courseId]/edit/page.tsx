9873a5d73e3f3aab1d25c6c35d94c26e91b6e400
Soroosh <acmaster777@yahoo.com>
2025-04-23 16:49:54 +0330

    feat: add course section create/update/delete functionality

--- src/app/admin/courses/[courseId]/edit/page.tsx
+++ src/app/admin/courses/[courseId]/edit/page.tsx
@@ -1,13 +1,20 @@
+import ActionButton from '@/components/ActionButton';
 import PageHeader from '@/components/PageHeader';
-import { Card, CardHeader } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { DialogTrigger } from '@/components/ui/dialog';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { db } from '@/drizzle/db';
 import { CourseSectionTable, CourseTable, LessonTable } from '@/drizzle/schema';
 import CourseForm from '@/features/courses/components/CourseForm';
 import { getCourseIdTag } from '@/features/courses/db/cache/courses';
+import { deleteSection } from '@/features/courseSections/actions/sections';
+import SectionFormDialog from '@/features/courseSections/components/SectionFormDialog';
 import { getCourseSectionCourseTag } from '@/features/courseSections/db/cache';
 import { getLessonCourseTag } from '@/features/lessons/db/cache/lessons';
+import { cn } from '@/lib/utils';
 import { asc, eq } from 'drizzle-orm';
+import { EyeClosedIcon, PlusIcon, Trash2Icon } from 'lucide-react';
 import { cacheTag } from 'next/dist/server/use-cache/cache-tag';
 import { notFound } from 'next/navigation';
 
@@ -25,7 +32,44 @@ async function EditCoursePage({ params }: { params: Promise<{ courseId: string }
           <TabsTrigger value="lessons">Lessons</TabsTrigger>
           <TabsTrigger value="details">Details</TabsTrigger>
         </TabsList>
-        <TabsContent value="lessons">Lessons</TabsContent>
+        <TabsContent value="lessons">
+          <Card>
+            <CardHeader className="flex items-center justify-between">
+              <CardTitle>Sections</CardTitle>
+              <SectionFormDialog courseId={course.id}>
+                <DialogTrigger asChild>
+                  <Button variant="outline">
+                    <PlusIcon /> New Section
+                  </Button>
+                </DialogTrigger>
+              </SectionFormDialog>
+            </CardHeader>
+            <CardContent>
+              {course.courseSections.map(section => (
+                <div key={section.id} className="flex items-center gap-1">
+                  <div className={cn('contents', section.status === 'private' && 'text-muted-foreground')}>
+                    {section.status === 'private' && <EyeClosedIcon className="size-4" />}
+                    {section.name}
+                  </div>
+                  <SectionFormDialog courseId={courseId} section={section}>
+                    <DialogTrigger asChild>
+                      <Button variant="outline" size="sm" className="ml-auto">Edit</Button>
+                    </DialogTrigger>
+                  </SectionFormDialog>
+                  <ActionButton
+                    action={deleteSection.bind(null, section.id)}
+                    requireAreYouSure
+                    variant="destructiveOutline"
+                    size="sm"
+                  >
+                    <Trash2Icon />
+                    <span className="sr-only">Delete</span>
+                  </ActionButton>
+                </div>
+              ))}
+            </CardContent>
+          </Card>
+        </TabsContent>
         <TabsContent value="details">
           <Card>
             <CardHeader>

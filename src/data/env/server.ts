61f3760f580fb937ca78191aaeb9dc93867b155e
Soroosh <acmaster777@yahoo.com>
2025-04-26 19:24:44 +0330

    feat: build consumer home page

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -11,6 +11,10 @@ export const env = createEnv({
     CLERK_WEBHOOK_SIGNING_SECRET: z.string().nonempty(),
     ARCJET_KEY: z.string().nonempty(),
     TEST_IP_ADDRESS: z.string().nonempty().optional(),
+    STRIPE_PPP_50_COUPON_ID: z.string().nonempty(),
+    STRIPE_PPP_40_COUPON_ID: z.string().nonempty(),
+    STRIPE_PPP_30_COUPON_ID: z.string().nonempty(),
+    STRIPE_PPP_20_COUPON_ID: z.string().nonempty(),
   },
   experimental__runtimeEnv: process.env,
 });
